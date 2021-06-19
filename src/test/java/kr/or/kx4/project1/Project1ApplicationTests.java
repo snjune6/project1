730b8390a8ffc68a7f9067f0cac3f7eb12971682
dev <snjune6@naver.com>
2021-06-19 21:10:39 +0900

    Initial commit

---
+++ src/test/java/kr/or/kx4/project1/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kx4.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
